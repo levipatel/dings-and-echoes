7db8b498376585048e62b63fb22a0cdf5b46486b
Levi Patel <levipatel@gmail.com>
2020-08-29 22:58:32 +1200

    Setup basic Tone synth with effects

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,11 +1,12 @@
 // based off https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
 import React, { useState, useRef, useEffect, Component } from 'react';
-import { Sampler } from "tone";
+import { Sampler, Synth, Filter, FeedbackDelay, now, Reverb } from "tone";
+import * as Tone from "tone"
 import A1 from "./audio/ding-1.wav";
 import B1 from "./audio/shaker-1.wav";
 
 const Canvas = props => {
-  
+
   const canvasRef = useRef(null)
   
   // TODO: move out of component, should be passed as a prop
@@ -26,8 +27,8 @@ const Canvas = props => {
   }, [draw])
 
   // TODO: move all tone generation to separate component/file
-  
-  //Setup for Sampler
+
+  //Testing Tone Sampler
   const [isLoaded, setLoaded] = useState(false);
   const sampler = useRef(null);
 
@@ -43,15 +44,32 @@ const Canvas = props => {
     ).toMaster();
   }, []);
 
-  function playTone(note) {
+  function playSamplerTone(note) {
     if (isLoaded){
       sampler.current.triggerAttack(note);
     }
   }
 
-  //Periodically call the two tones
-  setInterval( () => playTone("A1"), 3000);
-  setInterval( () => playTone("B1"), 5000);
+  //Periodically call the two audio samples
+  setInterval( () => playSamplerTone("A1"), 3000);
+  setInterval( () => playSamplerTone("B1"), 5000);
+
+  //Testing Tone synth
+  const synth = new Synth();
+  const now = Tone.now();
+
+  synth.triggerAttackRelease("C4", "8n", now);
+  synth.triggerAttackRelease("E4", "8n", now + 0.5);
+  synth.triggerAttackRelease("G4", "1m", now + 1);
+
+  const filter = new Filter(3000, 'lowpass');
+  const feedbackDelay = new FeedbackDelay("4n", 0.5);
+  const reverb = new Reverb(0.4).toDestination();
+
+  //Run a series of effects on the synth
+  synth.connect(filter);
+  filter.connect(feedbackDelay);
+  feedbackDelay.connect(reverb);
 
   return <canvas ref={canvasRef} {...props}/>
 }
