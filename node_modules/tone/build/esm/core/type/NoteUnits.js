222d8e456130bf5e1fe1b7c732244f73268b90b8
Levi Patel <levipatel@gmail.com>
2020-08-29 21:30:32 +1200

    Merge branch 'gh-pages' of https://github.com/levipatel/DingsAndEchoes into gh-pages

---
+++ node_modules/tone/build/esm/core/type/NoteUnits.js
@@ -0,0 +1,2 @@
+// this file contains all of the valid note names for all pitches between C-4 and C11
+//# sourceMappingURL=NoteUnits.js.map
---
+++ node_modules/tone/build/esm/version.js
@@ -0,0 +1,2 @@
+export const version = "14.7.39";
+//# sourceMappingURL=version.js.map
